c1875f44b3fd4456ca514eb141a72e025ce327e1
Kevin Kouketsu <kevinkouketsu53@gmail.com>
2024-05-11 18:12:31 -0300

    Bump version

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake, tools
 
 class EmulatorNetworkConan(ConanFile):
     name = "emulator_network"
-    version = "0.2.1"
+    version = "0.2.2"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
